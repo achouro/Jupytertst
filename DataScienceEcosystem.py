d01eab60a1c2a7540dd1840dcdc3822bd7fe3380
achouro <97984483+achouro@users.noreply.github.com>
2023-10-12 12:43:14 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+#     
+# 1. R
+# 2. Python
+# 3. SQL
+# 
+# 
+# 
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     
+# 1. ggplot2
+# 2. caret
+# 3. matplotlib
+
+# ## Data Science Tools
+# 
+# 1. RStudio
+# 2. Apache Spark
+# 3. TensorFlow
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[13]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[15]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200//3
+
+
+# **Objectives:**
+# - List popular languages for Data Science
+# - List commonly used libraries 
+# - List data science tools using heading 2
+# - Run arithmetic operation as code
+# - Convert minutes to hours as code
+
+# ## Author
+# Omar Achour
+
+# In[ ]:
+
+
+
+
